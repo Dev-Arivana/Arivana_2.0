2caa59bed5eb3761a4bc720c450fcba2c79b3e88
SAHIL CHAUDHARY <sahilchaudhary975832@gmail.com>
2025-10-28 11:59:14 +0530

    Clean up _document.js formatting and script tag
    Formatted import statement and removed unused script tag.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,15 @@
-import { Html, Head, Main, NextScript} from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        
         <link rel="icon" href="/logo.png" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/gsap.min.js" integrity="sha512-NcZdtrT77bJr4STcmsGAESr06BYGE8woZdSdEgqnpyqac7sugNO+Tr4bGwGF3MsnEkGKhU2KL2xh6Ec+BqsaHA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     </Html>
   );
 }
